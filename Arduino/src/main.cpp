b7215d3577e6e662ccc4f556dc040ecfc8d59e21
deluf <67149013+deluf@users.noreply.github.com>
2025-08-05 15:00:43 +0200

    Coded arduino to control L298N

--- Arduino/src/main.cpp
+++ Arduino/src/main.cpp
@@ -1,24 +1,76 @@
 
-#include <SPI.h>
-#include <RF24.h>
-#include <nRF24L01.h>
+#include <Arduino.h>
 
 #define BAUD_RATE 115200
-uint8_t block_buffer[32];
+
+#define STEER_PWM_PIN 8
+#define STEER_RIGHT_PIN 9
+#define STEER_LEFT_PIN 10
+#define MARCH_BACKWARDS_PIN 11
+#define MARCH_FORWARD_PIN 12
+#define MARCH_PWM_PIN 13
 
 void setup() 
 {
-	Serial.begin(BAUD_RATE);	
+	Serial.begin(BAUD_RATE);
+
+	pinMode(STEER_PWM_PIN, OUTPUT);
+	pinMode(STEER_RIGHT_PIN, OUTPUT);  
+  	pinMode(STEER_LEFT_PIN, OUTPUT);
+  	pinMode(MARCH_BACKWARDS_PIN, OUTPUT);
+	pinMode(MARCH_FORWARD_PIN, OUTPUT);
+	pinMode(MARCH_PWM_PIN, OUTPUT);
+
+	analogWrite(STEER_PWM_PIN, 0);
+	digitalWrite(STEER_RIGHT_PIN, LOW);
+	digitalWrite(STEER_LEFT_PIN, LOW);
+
+	analogWrite(MARCH_PWM_PIN, 0);
+	digitalWrite(MARCH_BACKWARDS_PIN, LOW);
+	digitalWrite(MARCH_FORWARD_PIN, LOW);
 }
+
+enum COMMAND 
+{
+	MARCH,
+	STEER
+};
+
+enum DIRECTION 
+{
+	FORWARD,
+	BACKWARDS,
+	RIGHT,
+	LEFT
+};
+
+uint8_t buffer[3];
 
 void loop() 
 {
 	int available_bytes = Serial.available();
-	if (available_bytes <= 0) { return; }
+	if (available_bytes < 3) { return; }
+	Serial.readBytes(buffer, 3);
 
-	Serial.readBytes(block_buffer, available_bytes);
+	// <command> <direction> <speed>
+	COMMAND command = (COMMAND)buffer[0];
+	DIRECTION direction = (DIRECTION)buffer[1];
+	uint8_t speed = buffer[2];
 
-	// Echo
-	while (Serial.availableForWrite() < available_bytes) { ; }
-	Serial.write(block_buffer, available_bytes);
+	if (command == MARCH) 
+	{
+		analogWrite(MARCH_PWM_PIN, speed);
+		digitalWrite(MARCH_FORWARD_PIN, direction == FORWARD);
+		digitalWrite(MARCH_BACKWARDS_PIN, direction == BACKWARDS);
+	}
+	else if (command == STEER) 
+	{
+		analogWrite(STEER_PWM_PIN, speed);
+		digitalWrite(STEER_RIGHT_PIN, direction == RIGHT);
+		digitalWrite(STEER_LEFT_PIN, direction == LEFT);
+	}
 }
+
+// DO NOT TOUCH
+//while (Serial.availableForWrite() < available_bytes) { ; }
+//Serial.write(block_buffer, available_bytes);
